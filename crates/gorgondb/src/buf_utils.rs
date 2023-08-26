18470746e6e3629fc14fe07ed12631110c9bff43
Julien Kauffmann <julienkauffmann85+github@gmail.com>
2023-08-25 20:44:05 -0400

    Added support for ledger cairns and changed format to allow a LTV type of stream reading

--- crates/gorgondb/src/buf_utils.rs
+++ crates/gorgondb/src/buf_utils.rs
@@ -1,11 +1,34 @@
 use std::io::{Read, Write};
 
-use byteorder::{ByteOrder, ReadBytesExt};
+use byteorder::{ByteOrder, ReadBytesExt, WriteBytesExt};
 
 /// Get the size, in bytes, of the minimum number of bytes necessary to represent the specified
 /// buffer size.
-pub(crate) fn buffer_size_len(buf_size: u64) -> u8 {
-    ((buf_size + 255) / 256) as u8
+pub(crate) fn buffer_size_len(mut buf_size: u64) -> u8 {
+    let mut res = 0;
+
+    while buf_size > 0 {
+        buf_size >>= 8;
+        res += 1;
+    }
+
+    res
+}
+
+/// Write a buffer size len only.
+pub(crate) fn write_buffer_size_len(
+    mut w: impl Write,
+    size_len: u8,
+    info_bits: u8,
+) -> std::io::Result<usize> {
+    assert!(size_len <= 0x3f, "size_len should be stricly less than 64");
+    assert!(info_bits <= 4, "info_bits should be stricly less than 4");
+
+    let size_len = size_len | info_bits << 6;
+
+    w.write_u8(size_len)?;
+
+    Ok(1)
 }
 
 /// Write a buffer size to the specified `Write`.
@@ -13,7 +36,7 @@ pub(crate) fn buffer_size_len(buf_size: u64) -> u8 {
 /// The buffer size is written using the lowest possibly number of bytes, using the
 /// least-significant bits to encode the buffer size length.
 ///
-/// The `info_bits` is a 4 bits-max value that can be used to convey additional information into
+/// The `info_bits` is a 2 bits-max value that can be used to convey additional information into
 /// the resulting value.
 ///
 /// If the buffer size to write is 0, the write is optimized and contains only the `info_bits`.
@@ -24,57 +47,80 @@ pub(crate) fn write_buffer_size(
     buf_size: u64,
     info_bits: u8,
 ) -> std::io::Result<usize> {
-    let mut size_buf = [0; 8];
-    byteorder::NetworkEndian::write_u64(&mut size_buf, buf_size);
-
     let size_len = buffer_size_len(buf_size);
+    write_buffer_size_len(&mut w, size_len, info_bits)?;
 
-    // The size should never take more than 8 bytes, ensuring that the buffer size never exceeds 64
-    // bits.
-    if size_len > 8 {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidData,
-            format!("size_len is too large ({size_len} > 8)"),
-        ));
+    if size_len > 0 {
+        // The size should never take more than 8 bytes, ensuring that the buffer size never exceeds 64
+        // bits.
+        assert!(size_len <= 8, "size_len is too large ({size_len} > 8)");
+
+        let mut size_buf = [0; 8];
+        byteorder::NetworkEndian::write_u64(&mut size_buf, buf_size);
+
+        let idx = size_buf.len() - size_len as usize;
+
+        w.write_all(&size_buf[idx..])?;
     }
-
-    let idx = size_buf.len() - size_len as usize;
-
-    assert!(info_bits <= 16, "info_bits should be stricly less than 16");
-
-    let size_len = size_len | info_bits << 4;
-
-    w.write_all(&[size_len])?;
-    w.write_all(&size_buf[idx..])?;
 
     Ok(size_len as usize + 1)
 }
 
-/// Read a buffer size from the specified `Read`.
-///
-/// Returns `None` if the buffer size is not present.
+/// Read a buffer size len from the specified `Read`.
 ///
 /// The second element of the returned tuple are the info bits.
-pub(crate) fn read_buffer_size(mut r: impl Read) -> std::io::Result<(Option<u64>, u8)> {
+pub(crate) fn read_buffer_size_len(mut r: impl Read) -> std::io::Result<(u8, u8)> {
     let size_len = r.read_u8()?;
 
-    let (info_bits, size_len) = (size_len >> 4, size_len & 0x0f);
+    Ok((size_len & 0x3f, size_len >> 6))
+}
 
+/// Read a buffer size from the specified `Read` using the specified size length.
+///
+/// The second element of the returned tuple are the info bits.
+pub(crate) fn read_buffer_size_with_size_len(
+    mut r: impl Read,
+    size_len: u8,
+) -> std::io::Result<u64> {
     if size_len == 0 {
-        return Ok((None, info_bits));
+        return Ok(0);
     }
 
     let mut size_buf = [0; 8];
 
-    if size_len as usize > size_buf.len() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidData,
-            "invalid size length",
-        ));
-    }
+    assert!(
+        size_len as usize <= size_buf.len(),
+        "size_len should never exceed {}",
+        size_buf.len()
+    );
 
     r.read_exact(&mut size_buf[8 - size_len as usize..])?;
     let size = byteorder::NetworkEndian::read_u64(&size_buf);
 
-    Ok((Some(size), info_bits))
+    Ok(size)
+}
+
+/// Read a buffer size from the specified `Read`.
+///
+/// The second element of the returned tuple are the info bits.
+pub(crate) fn read_buffer_size(mut r: impl Read) -> std::io::Result<(u64, u8)> {
+    let (size_len, info_bits) = read_buffer_size_len(&mut r)?;
+
+    read_buffer_size_with_size_len(r, size_len).map(|buf_size| (buf_size, info_bits))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_buffer_size_len() {
+        assert_eq!(buffer_size_len(0x00), 0); // 0
+        assert_eq!(buffer_size_len(0x01), 1); // 1
+        assert_eq!(buffer_size_len(0x02), 1); // 2
+        assert_eq!(buffer_size_len(0xff), 1); // 255
+        assert_eq!(buffer_size_len(0x100), 2); // 256
+        assert_eq!(buffer_size_len(0xffff), 2); // 65535
+        assert_eq!(buffer_size_len(0x010000), 3); // 65536
+    }
 }
--- crates/gorgondb/src/cairn.rs
+++ crates/gorgondb/src/cairn.rs
@@ -1,11 +1,18 @@
-use std::{fmt::Display, io::Write, str::FromStr};
+use std::{
+    fmt::Display,
+    io::{Read, Write},
+    str::FromStr,
+};
 
 use byteorder::ReadBytesExt;
 use bytes::Bytes;
 use serde_with::{DeserializeFromStr, SerializeDisplay};
 use thiserror::Error;
 
-use crate::{buf_utils, RemoteRef};
+use crate::{
+    buf_utils::{self, read_buffer_size_with_size_len},
+    RemoteRef,
+};
 
 /// An error type for [``Cairns``](``Cairn``).
 #[derive(Debug, Error)]
@@ -15,13 +22,16 @@ pub enum Error {
     InvalidCairn(String),
 }
 
+/// A convenience result type.
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
 /// A [``Cairn``](https://en.wikipedia.org/wiki/Cairn) is a marker for an unalterable piece of
 /// information - or put otherwise: an identifier for an immutable blob of data in GorgonDB.
 ///
 /// Cairns can be of different nature:
 ///
 /// - A self-contained ``Cairn`` contains the data itself, and is usually reserved for smaller
-/// blobs (think, 32 bytes or less). Self-contained cairns are special in that reading them is
+/// blobs (think, 63 bytes or less). Self-contained cairns are special in that reading them is
 /// simply reading from local memory and writing them is a no-op.
 /// - A remote-ref ``Cairn`` is a direct hash of the data it points to. This is the typical way of
 /// storing data in GorgonDB.
@@ -31,14 +41,21 @@ pub enum Error {
 pub enum Cairn {
     /// The ``Cairn`` contains the data directly.
     ///
-    /// Using this variant with data bigger than the recommended 32 bytes limit is ill-advised.
+    /// This variant cannot hold more than 256 bytes.
     SelfContained(Bytes),
 
     /// The ``Cairn`` is a hash to a blob of data stored elsewhere.
     RemoteRef(RemoteRef),
+
+    /// The ``Cairn`` is an aggregation of other cairns.
+    Ledger { ref_size: u64, cairn: Box<Self> },
 }
 
 impl Cairn {
+    const INFO_BITS_SELF_CONTAINED: u8 = 0x00;
+    pub(crate) const INFO_BITS_REMOTE_REF: u8 = 0x01;
+    const INFO_BITS_LEDGER: u8 = 0x02;
+
     /// Instantiate an empty ``Cairn``.
     pub fn empty() -> Self {
         Self::SelfContained(Default::default())
@@ -46,12 +63,20 @@ impl Cairn {
 
     /// Instantiate a new self-contained ``Cairn`` referencing the specified data.
     ///
-    /// No check is made ot ensure that the specified data is small enough. It is **highly
-    /// recommended** to never exceed 32 bytes for self-contained [``Cairns``](``Cairn``).
+    /// If the data exceed 63 bytes, an error is returned.
     ///
     /// The data will be copied from the slice.
-    pub fn self_contained(buf: impl Into<Bytes>) -> Self {
-        Self::SelfContained(buf.into())
+    pub fn self_contained(buf: impl Into<Bytes>) -> Result<Self> {
+        let buf = buf.into();
+
+        if buf.len() > 0x3f {
+            Err(Error::InvalidCairn(format!(
+                "self-contained cairn can never exceed {} bytes",
+                0x3f,
+            )))
+        } else {
+            Ok(Self::SelfContained(buf))
+        }
     }
 
     /// Get the size of the data referenced by this cairn.
@@ -60,8 +85,27 @@ impl Cairn {
             Self::SelfContained(raw) => raw
                 .len()
                 .try_into()
-                .expect("self-contained data size must be representable by a u64"),
+                .expect("self-contained data size should be representable by a u64"),
             Self::RemoteRef(remote_ref) => remote_ref.ref_size(),
+            Self::Ledger { ref_size, .. } => *ref_size,
+        }
+    }
+
+    /// Get the size of the resulting buffer when calling `to_vec` or `write_to`.
+    pub fn buf_len(&self) -> usize {
+        match self {
+            Self::SelfContained(raw) => {
+                buf_utils::buffer_size_len(
+                    raw.len()
+                        .try_into()
+                        .expect("self-contained data size should be representable by a u64"),
+                ) as usize
+                    + raw.len()
+            }
+            Self::RemoteRef(remote_ref) => remote_ref.buf_len(),
+            Self::Ledger { ref_size, cairn } => {
+                buf_utils::buffer_size_len(*ref_size) as usize + cairn.buf_len()
+            }
         }
     }
 
@@ -69,26 +113,83 @@ impl Cairn {
     pub fn write_to(&self, mut w: impl Write) -> std::io::Result<usize> {
         match self {
             Self::SelfContained(raw) => {
-                w.write_all(&[0x00])?;
+                let cnt = buf_utils::write_buffer_size_len(
+                    &mut w,
+                    raw.len()
+                        .try_into()
+                        .expect("self-contained data size should be representable by a u8"),
+                    Self::INFO_BITS_SELF_CONTAINED,
+                )?;
                 w.write_all(raw)?;
 
-                Ok(raw.len() + 1)
+                Ok(cnt + raw.len())
             }
             Self::RemoteRef(remote_ref) => remote_ref.write_to(w),
+            Self::Ledger { ref_size, cairn } => {
+                let cnt = buf_utils::write_buffer_size(&mut w, *ref_size, Self::INFO_BITS_LEDGER)?;
+
+                Ok(cairn.write_to(w)? + cnt)
+            }
         }
     }
 
     /// Return a vector of bytes representing the cairn in a non human-friendly way.
     pub fn to_vec(&self) -> Vec<u8> {
-        match self {
-            Self::SelfContained(raw) => {
-                let mut res = Vec::with_capacity(raw.len() + 1);
-                res.push(0x00u8);
-                res.extend_from_slice(raw);
+        let mut res = Vec::with_capacity(self.buf_len());
 
-                res
+        self.write_to(&mut res)
+            .expect("writing to a memory buffer should never fail");
+
+        res
+    }
+
+    /// Read a `Cairn` from the specified reader.
+    pub fn read_from(mut r: impl Read) -> std::io::Result<Self> {
+        match buf_utils::read_buffer_size_len(&mut r).map_err(|err| {
+            std::io::Error::new(err.kind(), format!("failed to read reference size: {err}"))
+        })? {
+            (raw_size, Self::INFO_BITS_SELF_CONTAINED) => {
+                let mut buf = vec![0x00; raw_size as usize];
+
+                r.read_exact(&mut buf).map_err(|err| {
+                    std::io::Error::new(err.kind(), "failed to read self-contained data")
+                })?;
+
+                Ok(Self::SelfContained(buf.into()))
             }
-            Self::RemoteRef(remote_ref) => remote_ref.to_vec(),
+            (size_len, Self::INFO_BITS_REMOTE_REF) => {
+                let ref_size = read_buffer_size_with_size_len(&mut r, size_len)?;
+                RemoteRef::read_without_header_from(ref_size, r).map(Self::RemoteRef)
+            }
+            (size_len, Self::INFO_BITS_LEDGER) => {
+                let ref_size = read_buffer_size_with_size_len(&mut r, size_len)?;
+                let cairn = Box::new(Cairn::read_from(r)?);
+
+                Ok(Self::Ledger { ref_size, cairn })
+            }
+            (_, info_bits) => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("unexpected info bits `{info_bits:02x}` for cairn"),
+            )),
+        }
+    }
+
+    /// Read the `Cairn` from the specified slice.
+    ///
+    /// If the slice contains extra data, an error is returned.
+    pub fn from_slice(buf: &[u8]) -> Result<Self> {
+        let mut r = std::io::Cursor::new(buf);
+
+        let res = Self::read_from(&mut r).map_err(|err| Error::InvalidCairn(err.to_string()))?;
+
+        match r.read_u8() {
+            Ok(b) => Err(Error::InvalidCairn(format!(
+                "unexpected byte after cairn: 0x{b:02x}"
+            ))),
+            Err(err) if err.kind() != std::io::ErrorKind::UnexpectedEof => Err(
+                Error::InvalidCairn(format!("unexpected error after cairn: {err}")),
+            ),
+            Err(_) => Ok(res),
         }
     }
 }
@@ -103,63 +204,11 @@ impl FromStr for Cairn {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let raw: Bytes = base85::decode(s)
-            .map_err(|err| Error::InvalidCairn(format!("failed to parse base85 string: {err}")))?
-            .into();
+        let raw = base85::decode(s)
+            .map_err(|err| Error::InvalidCairn(format!("failed to parse base85 string: {err}")))?;
 
-        raw.try_into()
-    }
-}
-
-impl TryFrom<Bytes> for Cairn {
-    type Error = Error;
-
-    fn try_from(raw: Bytes) -> Result<Self, Self::Error> {
-        let mut r = std::io::Cursor::new(&raw);
-
-        match buf_utils::read_buffer_size(&mut r)
-            .map_err(|err| Error::InvalidCairn(format!("failed to read reference size: {err}")))?
-        {
-            (Some(ref_size), info_bits) => {
-                let hash_algorithm = r
-                    .read_u8()
-                    .map_err(|err| Error::InvalidCairn(format!("failed to read algorithm: {err}")))?
-                    .try_into()
-                    .map_err(|err| {
-                        Error::InvalidCairn(format!("failed to parse algorithm: {err}"))
-                    })?;
-
-                let hash = raw.slice(r.position() as usize..);
-
-                let remote_ref = RemoteRef {
-                    ref_size,
-                    info_bits,
-                    hash_algorithm,
-                    hash,
-                };
-
-                Ok(Self::RemoteRef(remote_ref))
-            }
-            (None, _) => Ok(Self::SelfContained(raw.slice(1..))),
-        }
-    }
-}
-
-impl TryFrom<Vec<u8>> for Cairn {
-    type Error = Error;
-
-    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
-        let buf: Bytes = value.into();
-
-        buf.try_into()
-    }
-}
-
-impl TryFrom<&'_ [u8]> for Cairn {
-    type Error = Error;
-
-    fn try_from(value: &'_ [u8]) -> Result<Self, Self::Error> {
-        Bytes::copy_from_slice(value).try_into()
+        Self::read_from(std::io::Cursor::new(raw))
+            .map_err(|err| Error::InvalidCairn(err.to_string()))
     }
 }
 
@@ -179,8 +228,8 @@ mod tests {
     fn test_cairn_empty() {
         let expected = "00"; // Contains `vec[]`.
         let cairn = Cairn::empty();
+        assert_eq!(cairn, Cairn::SelfContained(Bytes::new()));
         assert_eq!(cairn.to_string(), expected);
-        assert!(matches!(cairn, Cairn::SelfContained(_)));
         assert_eq!(cairn.size(), 0);
 
         let cairn: Cairn = serde_json::from_value(json!(expected)).unwrap();
@@ -189,10 +238,10 @@ mod tests {
 
     #[test]
     fn test_cairn_self_contained() {
-        let expected = "009"; // Contains `vec[0x01]`.
+        let expected = "0Ra"; // Contains `vec[0x01]`.
         let cairn: Cairn = expected.parse().unwrap();
+        assert_eq!(cairn, Cairn::self_contained(vec![0x01]).unwrap());
         assert_eq!(cairn.to_string(), expected);
-        assert!(matches!(cairn, Cairn::SelfContained(_)));
         assert_eq!(cairn.size(), 1);
 
         let cairn: Cairn = serde_json::from_value(json!(expected)).unwrap();
@@ -201,11 +250,49 @@ mod tests {
 
     #[test]
     fn test_cairn_remote_ref() {
-        let expected = "0RaI>{Bj?=!E)e|U!r>PXC2}tx{`4;fGL=<3KeLfh-E7";
+        let expected = "K>-0s{Bj?=!E)e|U!r>PXC2}tx{`4;fGL=<3KeLfh-E7";
         let cairn: Cairn = expected.parse().unwrap();
-        assert_eq!(cairn.to_string(), expected);
         assert!(matches!(cairn, Cairn::RemoteRef(_)));
+        assert_eq!(cairn.to_string(), expected);
         assert_eq!(cairn.size(), 1);
+
+        let cairn: Cairn = serde_json::from_value(json!(expected)).unwrap();
+        assert_eq!(serde_json::to_value(cairn).unwrap(), json!(expected));
+    }
+
+    #[test]
+    fn test_cairn_ledger() {
+        let first = Cairn::self_contained(vec![0x01]).unwrap();
+        let second = Cairn::self_contained(vec![0x02]).unwrap();
+
+        let mut data = Vec::with_capacity(first.buf_len() + second.buf_len());
+        first.write_to(&mut data).unwrap();
+        second.write_to(&mut data).unwrap();
+        let inner = Cairn::self_contained(data).unwrap();
+
+        // [0x81, 0x02, 0x04, 0x01, 0x01, 0x01, 0x02]
+        //  ^     ^     ^     ^           ^
+        //  |     |     |     |           \- The `second` self-contained cairn.
+        //  |     |     |     |
+        //  |     |     |     \- The `first` self-contained cairn.
+        //  |     |     |
+        //  |     |     \- The `inner` cairn that contains, as its data, two self-contained
+        //  |     |        cairns.
+        //  |     |
+        //  |     \- The encoded size of the aggregated data: that's 1 + 1 = 2.
+        //  |
+        //  \- The ledger info bit (0x03 << 6) followed by the size of the size (1).
+        let expected = "fdT{p0RaL";
+        let cairn: Cairn = expected.parse().unwrap();
+        assert_eq!(
+            cairn,
+            Cairn::Ledger {
+                ref_size: 2,
+                cairn: Box::new(inner),
+            },
+        );
+        assert_eq!(cairn.to_string(), expected);
+        assert_eq!(cairn.size(), 2);
 
         let cairn: Cairn = serde_json::from_value(json!(expected)).unwrap();
         assert_eq!(serde_json::to_value(cairn).unwrap(), json!(expected));
